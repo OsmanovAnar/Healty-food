e1ad630fa60de93168af0225759d7ecb0f11f512
Anar Osmanov <anar_osmanov@vk.com>
2025-09-26 18:40:27 +0400

    Добавил раздел breakfast

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -2,6 +2,7 @@ import Section1 from './sections/section-1/section-1'
 import Section2 from './sections/section-2/Section-2'
 import Section3 from './sections/section-3/section-3'
 import Section4 from './sections/section-4/section-4'
+import Section5 from './sections/section-5/section-5'
 
 const Main = () => {
   return (
@@ -10,6 +11,7 @@ const Main = () => {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section5 />
     </main>
   )
 }
---
+++ src/main/sections/section-5/section-5.tsx
@@ -0,0 +1,16 @@
+import Breakfast from "./breakfast"
+
+const Section5 = () => {
+  return (
+    <div className="h-[840px] w-[1110px] bg-amber-200">
+
+<Breakfast />
+
+
+
+
+    </div>
+  )
+}
+
+export default Section5
