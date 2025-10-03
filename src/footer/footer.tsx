1cf507cba62ed67ea8f6ccf0cb9e37b80cf38a24
Anar Osmanov <anar_osmanov@vk.com>
2025-10-03 18:19:08 +0400

    Мелкая доработка футера

--- src/footer/footer.tsx
+++ src/footer/footer.tsx
@@ -3,23 +3,21 @@ import { Clock, MapPin, Phone } from 'lucide-react'
 const Footer = () => {
   return (
     <footer className="flex h-[275px] w-full flex-col items-center justify-center gap-[62px] bg-[#252525]">
-      <div>
-        <div className="mt-[70px] flex items-center justify-center gap-[90px] text-white">
-          <div className="flex flex-col items-center">
-            <Clock className="mb-[16px]" />
-            <div>Today 10:00 am - 7:00 pm</div>
-            <div>Working hours</div>
-          </div>
-          <div className="flex flex-col items-center">
-            <MapPin className="mb-[16px]" />
-            <div>Velyka Vasylkivska 100</div>
-            <div>Get Directions</div>
-          </div>
-          <div className="flex flex-col items-center">
-            <Phone className="mb-[16px]" />
-            <div>+38 (063)833 24 15</div>
-            <div>Call Online</div>
-          </div>
+      <div className="mx-auto mt-[70px] flex w-full max-w-[915px] items-center justify-between text-white">
+        <div className="flex flex-col items-center">
+          <Clock className="mb-[16px]" />
+          <div>Today 10:00 am - 7:00 pm</div>
+          <div>Working hours</div>
+        </div>
+        <div className="flex flex-col items-center">
+          <MapPin className="mb-[16px]" />
+          <div>Velyka Vasylkivska 100</div>
+          <div>Get Directions</div>
+        </div>
+        <div className="flex flex-col items-center">
+          <Phone className="mb-[16px]" />
+          <div>+38 (063)833 24 15</div>
+          <div>Call Online</div>
         </div>
       </div>
 
